2ea607e5f41778daa383521d24b7a6903a59bce4
hannan-abdul <shohas563@gmail.com>
2021-05-02 16:28:10 +0600

    responsive

--- src/components/Home/HomeProjectCard.js
+++ src/components/Home/HomeProjectCard.js
@@ -3,7 +3,7 @@ import './Home.css'
 
 const HomeProjectCard = ({ detail }) => {
     return (
-        <div className="col-md-4 my-5 main-card-body" >
+        <div className="col-md-6 col-sm-12 my-5 main-card-body" >
             <img className="work-img" style={{ width: '100%', height: '30%' }} src={detail.img} alt="" />
             <div className="card-body-home">
                 <div className='card-inner-detail'>
